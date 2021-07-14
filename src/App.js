e049d8068912710d3c30237311787952917c6829
beksiazek <beksiazek@gmail.com>
2021-07-14 12:07:02 -0300

    first edit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Central TCG by <i>bksiazek</i> coming soon.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/beksiazek/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out my Github profile
         </a>
       </header>
     </div>
